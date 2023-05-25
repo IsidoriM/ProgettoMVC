924656742b2e74b89efe802cd4486d1b499dc95f
IsidoriM <m.isidori@gsnetitalia.it>
2023-05-25 15:14:04 +0200

    Costruzione Collection

--- WebCorso/BLL/Graph.cs
+++ WebCorso/BLL/Graph.cs
@@ -1,7 +1,10 @@
-﻿using OCM.DatiGraph;
+﻿using Newtonsoft.Json;
+using OCM.DatiGraph;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
+
 
 namespace BLL
 {
@@ -9,10 +12,27 @@ namespace BLL
     {
         public List<GrandRecord> ImpostaRecord()
         {
-            List<GrandRecord> grandRecords = new List<GrandRecord>();
+            List<ChildRecord> R3 = new List<ChildRecord>() {
+                new ChildRecord(){ Id = 3, RecordId = 2,Name="(A)A"},
+                 new ChildRecord(){ Id = 0, RecordId = 2,Name="(A)A"},
 
+            };
+
+            IList<Record> R2 = new List<Record>() {
+                new Record(){ Id = 2, GrandRecordId = 1,Name="(A)A",ChildRecords=R3},
+
+            };
+
+            List<GrandRecord> R1 = new List<GrandRecord>() {
+                new GrandRecord(){ Id = 1, Name="(A)",Records=R2,},
+                
+            };
+
+           
+
+            
             var id = int.MinValue;
-            foreach (var grandRecord in grandRecords)
+            foreach (var grandRecord in R1)
             {
                 if (grandRecord.Id == 0)
                     grandRecord.Id = id++;
@@ -34,7 +54,7 @@ namespace BLL
                 }
             }
 
-            return grandRecords;
+            return R1;
         }
     }
 }
